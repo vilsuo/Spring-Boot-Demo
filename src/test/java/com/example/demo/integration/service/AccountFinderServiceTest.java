f90ea7cbaf17690aa646cf433b507fb033ec1a4e
ville suokas <vilsuo1@gmail.com>
2023-08-12 15:21:09 +0300

    Improved AccountFinderServiceTest: replaced accountcreationdto creation and testing with stream, added more variables to single test, renamed test to be more descriptive and added more tests

--- src/test/java/com/example/demo/integration/service/AccountFinderServiceTest.java
+++ src/test/java/com/example/demo/integration/service/AccountFinderServiceTest.java
@@ -1,6 +1,7 @@
 
 package com.example.demo.integration.service;
 
+import com.codepoetics.protonpack.StreamUtils;
 import com.example.demo.datatransfer.AccountCreationDto;
 import com.example.demo.domain.Account;
 import com.example.demo.domain.Role;
@@ -12,6 +13,8 @@ import com.example.demo.unit.UsernameValidatorTest;
 import jakarta.transaction.Transactional;
 import java.util.HashSet;
 import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -20,11 +23,19 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.junitpioneer.jupiter.cartesian.CartesianTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+/*
+TODO
+- move helper functions to a helper class
+	- no more username/password lists in this class
+*/
 @ActiveProfiles("test")
 @ExtendWith(SpringExtension.class)
 @Transactional
@@ -34,13 +45,7 @@ public class AccountFinderServiceTest {
 	private final List<String> VALID_USERNAMES = UsernameValidatorTest.VALID_USERNAMES;
 	private final List<String> VALID_PASSWORDS = PasswordValidatorTest.VALID_PASSWORDS;
 	
-	private final String username1 = VALID_USERNAMES.get(0);
-	private final String username2 = VALID_USERNAMES.get(1);
-	
-	private final String password1 = VALID_PASSWORDS.get(0);
-	private final String password2 = VALID_PASSWORDS.get(1);
-	
-	private final long id = 1l;
+	private final Long initiallyNotTakenId = 1l;
 	
 	@Autowired
 	private AccountFinderService accountFinderService;
@@ -49,249 +54,360 @@ public class AccountFinderServiceTest {
 	private AccountCreatorService accountCreatorService;
 	
 	@Test
-	public void findByIdNullThrowsTest() {
+	public void findingAccountByNullIdThrowsTest() {
 		assertThrows(
 			IllegalArgumentException.class,
 			() -> accountFinderService.findById(null),
-			"Serching Account by null id does not throw "
-			+ "IllegalArgumentException"
+			"Searching Account by null id does not throw"
 		);
 	}
 	
 	@Test
-	public void findByIdWhenIdIsNotFoundThrowsTest() {
+	public void findingAccountByIdThatDoesNotExistThrowsTest() {
 		assertThrows(
 			ResourceNotFoundException.class,
-			() -> accountFinderService.findById(id),
+			() -> accountFinderService.findById(initiallyNotTakenId),
 			"Searching Account by id that is not found from the database "
-			+ "does not throw ResourceNotFoundException"
+			+ "does not throw"
 		);	
 	}
 	
-	@Test
-	public void findByIdDoesNotThrowWhenIdExists() {
-		Account account = accountCreatorService.create(
-			new AccountCreationDto(username1, password1), Role.USER
-		).get();
-		
-		assertDoesNotThrow(
-			() -> accountFinderService.findById(account.getId()),
-			"When finding the created Account by id the method throws"
-		);
+	@ParameterizedTest
+	@EnumSource(Role.class)
+	public void findingAccountByCreatedAccountsIdDoesNotThrowTest(final Role role) {
+		accountCreationDtosWithUniqueUsernamesStream().forEach(accountCreationDto -> {
+			Account account = accountCreatorService.create(
+				accountCreationDto, role
+			).get();
+			
+			assertDoesNotThrow(
+				() -> accountFinderService.findById(account.getId()),
+				"When finding the created "
+				+ accountInfo(accountCreationDto, role)
+				+ " by id the method throws"
+			);
+		});
 	}
 	
-	@Test
-	public void findByIdAfterCreatingAccountFindsTheCorrectOneTest() {
-		Account account1 = accountCreatorService.create(
-			new AccountCreationDto(username1, password1), Role.USER
+	@CartesianTest
+	public void findingAccountsByDifferentIdsFindUnEqualAccountsTest(
+			@CartesianTest.Enum Role role1, @CartesianTest.Enum Role role2) {
+		
+		final AccountCreationDto accountCreationDto1
+			= accountCreationDtosWithUniqueUsernamesStream().findFirst().get();
+		
+		final Account account1 = accountCreatorService.create(
+			accountCreationDto1, role1
 		).get();
 		
-		Account account2 = accountCreatorService.create(
-			new AccountCreationDto(username2, password2), Role.ADMIN
-		).get();
-		
-		Account accountFound1 = accountFinderService.findById(
+		final Account accountFound1 = accountFinderService.findById(
 			account1.getId()
 		);
-		Account accountFound2 = accountFinderService.findById(
-			account2.getId()
-		);
 		
-		assertNotEquals(
-			accountFound1, accountFound2, "Different ids find the same Account"
-		);
-		
-		assertEquals(
-			account1, accountFound1,
-			"The first created Account differs from the found one"
-		);
-		
-		assertEquals(
-			account2, accountFound2,
-			"The second created Account differs from the found one"
-		);
+		accountCreationDtosWithUniqueUsernamesStream()
+			.skip(1) // skip the first one
+			.forEach(accountCreationDto2 -> {
+				Account account2 = accountCreatorService.create(
+					accountCreationDto2, role2
+				).get();
+
+				Account accountFound2 = accountFinderService.findById(
+					account2.getId()
+				);
+
+				assertNotEquals(
+					accountFound1, accountFound2, 
+					"Finding Accounts by the ids of Accounts created from "
+					+ accountInfo(accountCreationDto1, role1) + " and "
+					+ accountInfo(accountCreationDto2, role2)
+					+ " results in finding equal Accounts "
+					+ accountInfo(account1) + " and " + accountInfo(account2)
+				);
+			});
+	}
+	
+	@ParameterizedTest
+	@EnumSource(Role.class)
+	public void findingAccountByCreatedAccountsIdFindsTheCreatedAccountTest(final Role role) {
+		accountCreationDtosWithUniqueUsernamesStream().forEach(accountCreationDto -> {
+			Account account = accountCreatorService.create(
+				accountCreationDto, role
+			).get();
+			
+			Account accountFound = accountFinderService.findById(
+				account.getId()
+			);
+
+			assertEquals(
+				account, accountFound,
+				"The first created " + accountInfo(account)
+				+ " differs from the found one " + accountInfo(accountFound)
+			);
+		});
 	}
 	
 	@Test
-	public void findByUsernameNullThrowsTest() {
+	public void findingAccountByUsernameThrowsTest() {
 		assertThrows(
 			IllegalArgumentException.class,
-			() -> accountFinderService.findById(null),
-			"Serching Account by null username does not throw "
-			+ "IllegalArgumentException"
+			() -> accountFinderService.findByUsername(null),
+			"Finding Account by null username does not throw"
 		);
 	}
 	
 	@Test
-	public void findByUsernameWhenUsernameIsNotFoundThrowsTest() {
-		assertThrows(
-			ResourceNotFoundException.class,
-			() -> accountFinderService.findByUsername(username1),
-			"Searching Account by username that is not found from the database "
-			+ "does not throw ResourceNotFoundException"
-		);	
+	public void findingAccountByUsernameThatDoesNotExistThrows() {
+		accountCreationDtosWithUniqueUsernamesStream().forEach(accountCreationDto -> {
+			final String username = accountCreationDto.getUsername();
+			assertThrows(
+				ResourceNotFoundException.class,
+				() -> accountFinderService.findByUsername(username),
+				"Finding Account by username '" + username + "' that is not "
+				+ "created does not throw"
+			);
+		});
 	}
 	
-	@Test
-	public void findByUsernameDoesNotThrowWhenUsernameExists() {
-		Account account = accountCreatorService.create(
-			new AccountCreationDto(username1, password1), Role.USER
+	@ParameterizedTest
+	@EnumSource(Role.class)
+	public void findingAccountByCreatedAccountsUsernameDoesNotThrowTest(final Role role) {
+		accountCreationDtosWithUniqueUsernamesStream().forEach(accountCreationDto -> {
+			accountCreatorService.create(accountCreationDto, role);
+			
+			assertDoesNotThrow(
+				() -> accountFinderService.findByUsername(
+					accountCreationDto.getUsername()
+				),
+				"When finding " + accountInfo(accountCreationDto, role)
+				+ "by username, the method throws"
+			);
+		});
+	}
+	
+	@CartesianTest
+	public void findingAccountsByDifferentUsernamesFindUnEqualAccountsTest(
+			@CartesianTest.Enum Role role1, @CartesianTest.Enum Role role2) {
+		
+		final AccountCreationDto accountCreationDto1
+			= accountCreationDtosWithUniqueUsernamesStream().findFirst().get();
+		
+		final Account account1 = accountCreatorService.create(
+			accountCreationDto1, role1
 		).get();
 		
-		assertDoesNotThrow(
-			() -> accountFinderService.findByUsername(account.getUsername()),
-			"When finding the created Account by username the method throws"
+		final Account accountFound1 = accountFinderService.findByUsername(
+			accountCreationDto1.getUsername()
 		);
+		
+		accountCreationDtosWithUniqueUsernamesStream()
+			.skip(1) // skip the first one
+			.forEach(accountCreationDto2 -> {
+				Account account2 = accountCreatorService.create(
+					accountCreationDto2, role2
+				).get();
+
+				Account accountFound2 = accountFinderService.findByUsername(
+					accountCreationDto2.getUsername()
+				);
+
+				assertNotEquals(
+					accountFound1, accountFound2, 
+					"Finding Accounts by the usernames of Accounts created "
+					+ "from " + accountInfo(accountCreationDto1, role1)
+					+ " and " + accountInfo(accountCreationDto2, role2)
+					+ " results in finding equal Accounts "
+					+ accountInfo(account1) + " and " + accountInfo(account2)
+				);
+			});
+	}
+	
+	@ParameterizedTest
+	@EnumSource(Role.class)
+	public void findingAccountByCreatedAccountsUsernameFindsTheCreatedAccountTest(final Role role) {
+		accountCreationDtosWithUniqueUsernamesStream().forEach(accountCreationDto -> {
+			Account account = accountCreatorService.create(
+				accountCreationDto, role
+			).get();
+			
+			Account accountFound = accountFinderService.findByUsername(
+				account.getUsername()
+			);
+			
+			assertEquals(
+				account, accountFound,
+				"The first created " + accountInfo(account)
+				+ " differs from the found one " + accountInfo(accountFound)
+			);
+		});
 	}
 	
 	@Test
-	public void findByUsernameAfterCreatingAccountFindsTheCorrectOneTest() {
-		Account account1 = accountCreatorService.create(
-			new AccountCreationDto(username1, password1), Role.USER
-		).get();
-		
-		Account account2 = accountCreatorService.create(
-			new AccountCreationDto(username2, password2), Role.ADMIN
-		).get();
-		
-		Account accountFound1 = accountFinderService.findByUsername(
-			account1.getUsername()
-		);
-		Account accountFound2 = accountFinderService.findByUsername(
-			account2.getUsername()
-		);
-		
-		assertNotEquals(
-			accountFound1, accountFound2, 
-			"Different usernames find the same Account"
-		);
-		
-		assertEquals(
-			account1, accountFound1,
-			"The first created Account differs from the found one"
-		);
-		
-		assertEquals(
-			account2, accountFound2,
-			"The second created Account differs from the found one"
-		);
-	}
-	
-	@Test
-	public void existsByUsernameNullThrowsTest() {
+	public void findingIfNullUsernameExistsThrowsTest() {
 		assertThrows(
 			IllegalArgumentException.class,
 			() -> accountFinderService.existsByUsername(null),
-			"Searching if Account exists by null username does not throw "
-			+ "IllegalArgumentException"
+			"Searching if Account exists by null username does not throw"
 		);
 	}
 	
 	@Test
-	public void existsByUsernameDoesNotFindUsernameThatDoesNotExistTest() {
-		assertFalse(
-			accountFinderService.existsByUsername(username1),
-			"Username exists even when no Accounts are created"
-		);
+	public void ifThereNoAccountsCreatedWithGivenUsernameThenThatUsernameDoesNotExistTest() {
+		accountCreationDtosWithUniqueUsernamesStream().forEach(accountCreationDto -> {
+			final String username = accountCreationDto.getUsername();
+			assertFalse(
+				accountFinderService.existsByUsername(username),
+				"Username '" + username + "' exists even when no Accounts are "
+				+ "created"
+			);
+		});
+	}
+	
+	@ParameterizedTest
+	@EnumSource(Role.class)
+	public void ifAccountIsCreatedWithGivenUsernameThenThatUsernameExistsTest(final Role role) {
+		accountCreationDtosWithUniqueUsernamesStream().forEach(accountCreationDto -> {
+			accountCreatorService.create(accountCreationDto, role);
+
+			assertTrue(
+				accountFinderService.existsByUsername(
+					accountCreationDto.getUsername()
+				),
+				accountInfo(accountCreationDto, role) + " does not exist"
+			);
+		});
 	}
 	
 	@Test
-	public void existsByUsernameTakenUsernameIsFoundTest() {
-		final String username = username1;
-		accountCreatorService.create(
-			new AccountCreationDto(username, password1), Role.USER
-		);
-		
-		assertTrue(
-			accountFinderService.existsByUsername(username),
-			"Username is not taken when an Account is created with the username"
-		);
-	}
-	
-	@Test
-	public void listIsEmptyTest() {
+	public void accountListIsEmptyIfNoAccountsAreCreatedTest() {
 		assertTrue(
 			accountFinderService.list().isEmpty(),
 			"Account list is not empty before creating any Accounts"
 		);
 	}
 	
-	@Test
-	public void listCreatingAccountsWithUniqueUsernamesIncrementTheListSizeTest() {
-		accountCreatorService.create(
-			new AccountCreationDto(username1, password1), Role.USER
-		);
+	@ParameterizedTest
+	@EnumSource(Role.class)
+	public void creatingAccountsWithUsernamesThatAreNotTakenIncrementsTheAccountListSizeTest(final Role role) {
+		StreamUtils.zipWithIndex(accountCreationDtosWithUniqueUsernamesStream())
+			.forEach(
+				indexed -> {
+					AccountCreationDto accountCreationDto = indexed.getValue();
+					accountCreatorService.create(accountCreationDto, role);
+					
+					final Long created = indexed.getIndex() + 1;
+					assertEquals(
+						created, accountFinderService.list().size(),
+						"After creating " + created + " Accounts, the list "
+						+ "size was not incremented. The last created was "
+						+ accountInfo(accountCreationDto, role)
+					);
+				}
+			);
+	}
+	
+	/*
+	This method uses the same accountcreationdto twice in each iteration, thus
+	the username and PASSWORD are the same. The test is run with different Roles
+	*/
+	@CartesianTest
+	public void creatingAccountsWithUsernamesThatAreAlreadyTakenDoesNotIncrementTheAccountListSizeTest(
+			@CartesianTest.Enum Role role1, @CartesianTest.Enum Role role2) {
 		
-		assertEquals(
-			1, accountFinderService.list().size(),
-			"After creating the first Account, the list size is not incremented"
-		);
-		
-		accountCreatorService.create(
-			new AccountCreationDto(username2, password2), Role.ADMIN
-		);
-		
-		assertEquals(
-			2, accountFinderService.list().size(),
-			"After creating the second Account, the list size is not incremented"
+		StreamUtils.zipWithIndex(accountCreationDtosWithUniqueUsernamesStream())
+			.forEach(
+				indexed -> {
+					AccountCreationDto accountCreationDto = indexed.getValue();
+					accountCreatorService.create(accountCreationDto, role1);
+					
+					final Long created = indexed.getIndex() + 1;
+					assertEquals(
+						created, accountFinderService.list().size(),
+						"After creating the first "
+						+ accountInfo(accountCreationDto, role1)
+						+ ", the list size was not incremented"
+					);
+					
+					accountCreatorService.create(accountCreationDto, role2);
+
+					assertEquals(
+						created, accountFinderService.list().size(),
+						"After creating the second Account from the same "
+						+ "AccountCreationDto, the list size was incremented. "
+						+ "The last was created from "
+						+ accountInfo(accountCreationDto, role2)
+					);
+				}
+			);
+	}
+	
+	@ParameterizedTest
+	@EnumSource(Role.class)
+	public void afterCreatingAccountThatAccountCanBeFoundFromTheAccountsListTest(final Role role) {
+		accountCreationDtosWithUniqueUsernamesStream().forEach(accountCreationDto -> {
+			Account account = accountCreatorService.create(
+				accountCreationDto, role
+			).get();
+			
+			assertTrue(
+				accountFinderService.list().contains(account),
+				accountInfo(accountCreationDto, role) + " can not be found "
+				+ "in the list"
+			);
+		});
+	}
+	
+	@ParameterizedTest
+	@EnumSource(Role.class)
+	public void ifAccountIsNotCreatedThenThatAccountCanNotBeFoundFromTheAccountListTest(final Role role) {
+		accountCreationDtosWithUniqueUsernamesStream().forEach(accountCreationDto -> {
+			Account account = new Account(
+				accountCreationDto.getUsername(),
+				accountCreatorService.encodePassword(
+					accountCreationDto.getPassword()
+				),
+				role,
+				new HashSet<>(), new HashSet<>(), new HashSet<>()
+			);
+
+			assertFalse(
+				accountFinderService.list().contains(account),
+				accountInfo(accountCreationDto, role)
+				+ " that has not saved to the database can be found in the list"
+			);
+		});
+	}
+	
+	/**
+	 * 
+	 * @return Stream of AccountCreationDto objects.
+	 * 
+	 * The ALl AccountCreationDto objects have unique username chosen from
+	 * VALID_USERNAMEs list. There are as many objects in this stream as there
+	 * are usernames in the list. Passwords are chosen from the VALID_PASSWORDs 
+	 * list. All passwords are not guaranteed to be unique.
+	 */
+	private Stream<AccountCreationDto> accountCreationDtosWithUniqueUsernamesStream() {
+		return IntStream.range(0, VALID_USERNAMES.size())
+			.mapToObj(i -> new AccountCreationDto(
+				VALID_USERNAMES.get(i), getValidPasswordModuloSize(i)
+			)
 		);
 	}
 	
-	@Test
-	public void listCreatingAccountsWithDublicateUsernamesDoesNotIncrementTheListSizeTest() {
-		final String commonUsername = username1;
-		accountCreatorService.create(
-			new AccountCreationDto(commonUsername, password1), Role.USER
-		);
-		
-		assertEquals(
-			1, accountFinderService.list().size(),
-			"After creating the first Account, the list size is not incremented"
-		);
-		
-		accountCreatorService.create(
-			new AccountCreationDto(commonUsername, password2), Role.ADMIN
-		);
-		
-		assertEquals(
-			1, accountFinderService.list().size(),
-			"After creating the second Account with the same username as the "
-			+ "first one, the list size changes"
-		);
+	private String getValidPasswordModuloSize(int i) {
+		return VALID_PASSWORDS.get(i % VALID_PASSWORDS.size());
 	}
 	
-	@Test
-	public void listCreatedAccountsCanBeFoundInTheListTest() {
-		Account account1 = accountCreatorService.create(
-			new AccountCreationDto(username1, password1), Role.USER
-		).get();
-		
-		assertTrue(
-			accountFinderService.list().contains(account1),
-			"The first created Account can not be found in the list"
-		);
-		
-		Account account2 = accountCreatorService.create(
-			new AccountCreationDto(username2, password2), Role.ADMIN
-		).get();
-		
-		assertTrue(
-			accountFinderService.list().contains(account2),
-			"The second created Account can not be found in the list"
-		);
+	private String accountInfo(AccountCreationDto accountCreationDto, Role role) {
+		return "AccountCreationDto with username '"
+			+ accountCreationDto.getUsername()
+			+ "', raw password '" + accountCreationDto.getPassword()
+			+ "' and role '" + role.getName() + "'";
 	}
 	
-	@Test
-	public void listNotCreatedAccountCanNotBeFoundFromTheListTest() {
-		Account account = new Account(
-			username1, password1, Role.USER,
-			new HashSet<>(), new HashSet<>(), new HashSet<>()
-		);
-		
-		assertFalse(
-			accountFinderService.list().contains(account),
-			"Account that has not saved to the database can be found from "
-			+ " Account list"
-		);
+	private String accountInfo(Account account) {
+		return "Account with username '" + account.getUsername()
+			+ "', password '" + account.getPassword()
+			+ "' and role '" + account.getRole().getName() + "'";
 	}
 }
