eb08a679c13125dc3179fd688c7658d3efb5e0e5
Ville Suokas <vilsuo1@gmail.com>
2023-09-06 20:18:48 +0300

    changed validator to autowired bean validator and extended with SpringBootTest

--- src/test/java/com/example/demo/unit/AccountCreationDtoValidationTest.java
+++ src/test/java/com/example/demo/unit/AccountCreationDtoValidationTest.java
@@ -3,18 +3,25 @@ package com.example.demo.unit;
 
 import com.example.demo.datatransfer.AccountCreationDto;
 import jakarta.validation.ConstraintViolation;
-import jakarta.validation.Validation;
 import jakarta.validation.Validator;
 import java.util.Set;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static com.example.demo.testhelpers.helpers.AccountCreationHelper.uniqueAccountCreationDtoStream;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ActiveProfiles("test")
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
 public class AccountCreationDtoValidationTest {
 	
-	private final Validator validator
-			= Validation.buildDefaultValidatorFactory().getValidator();
+	@Autowired
+    private Validator validator;
 	
 	@Test
 	public void invalidUsernameCausesValidationErrorTest() {
